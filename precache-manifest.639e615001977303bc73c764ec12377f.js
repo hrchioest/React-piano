dc62edbaf32915f70417d6a5698042a55f4bf57a
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-22 18:54:49 -0300

    Updates

---
+++ precache-manifest.639e615001977303bc73c764ec12377f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d4736b7b05144dfe3e8f33500d8531",
+    "url": "/React-piano/index.html"
+  },
+  {
+    "revision": "fd09272de64382025796",
+    "url": "/React-piano/static/css/main.1115d556.chunk.css"
+  },
+  {
+    "revision": "7f32e5fd7d0c37eed76c",
+    "url": "/React-piano/static/js/2.937963ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-piano/static/js/2.937963ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd09272de64382025796",
+    "url": "/React-piano/static/js/main.a631b6ce.chunk.js"
+  },
+  {
+    "revision": "8ab932cfd6fa86182898",
+    "url": "/React-piano/static/js/runtime-main.8b100038.js"
+  },
+  {
+    "revision": "18fd3f6f1a6879d60b471eb4828bd890",
+    "url": "/React-piano/static/media/do.18fd3f6f.wav"
+  },
+  {
+    "revision": "a787534ad7a68707ce5c7657c0a26985",
+    "url": "/React-piano/static/media/fa.a787534a.wav"
+  },
+  {
+    "revision": "10af03f3aa8a23a3e29cab5bb9f7d48f",
+    "url": "/React-piano/static/media/la.10af03f3.wav"
+  },
+  {
+    "revision": "1f26c778a049229d1c7a2edfb789b5fd",
+    "url": "/React-piano/static/media/mi.1f26c778.wav"
+  },
+  {
+    "revision": "217b943a565a4c60e452aac2852fd0e9",
+    "url": "/React-piano/static/media/re.217b943a.wav"
+  },
+  {
+    "revision": "70924435d531fe3a2c59d6596733c5cc",
+    "url": "/React-piano/static/media/si.70924435.wav"
+  },
+  {
+    "revision": "785a33d2df8ac21f71be64df8f8f579a",
+    "url": "/React-piano/static/media/sol.785a33d2.wav"
+  }
+]);
