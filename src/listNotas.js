758bf2bde8937b65204c2773d7aa21073d841cda
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-22 18:28:02 -0300

    Se hace un archivo aparte de toda la información por cada tecla o nota

---
+++ src/listNotas.js
@@ -0,0 +1,46 @@
+
+const notas = [
+    {
+      nombre: "DO",
+      link: require("./notes/do.wav"),
+      tieneSostenido: true,
+      key:"1" 
+    },
+    {
+      nombre: "RE",
+      link: require("./notes/re.wav"),
+      tieneSostenido: true,
+      key:"2" 
+    },
+    {
+      nombre: "MI",
+      link: require("./notes/mi.wav"),
+      key:"3" 
+    },
+    {
+      nombre: "FA",
+      link: require("./notes/fa.wav"),
+      tieneSostenido: true,
+      key:"4" 
+    },
+    {
+      nombre: "SOL",
+      link: require("./notes/sol.wav"),
+      tieneSostenido: true,
+      key:"5" 
+    },
+    {
+      nombre: "LA",
+      link: require("./notes/la.wav"),
+      tieneSostenido: true,
+      key:"6" 
+    },
+    {
+      nombre: "SI",
+      link: require("./notes/si.wav"),
+      key:"7"
+    }
+  ];
+
+  export default notas;
+
