e9d20676259163e3c68c9463f71a8d85d55b6609
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-22 18:26:10 -0300

    Se agrega la estructura de la página con los eventos por cada nota del componente Key que es cada teclado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Key from "./Key";
+import notas from "./listNotas";
 
 function App() {
+  const [estaSonando, setEstaSonando] = useState("");
+  
+  const sonidoNota =(nota)=>{
+    const sonido = new Audio(nota.link);
+        sonido.play();
+  }
+
+  const handleClick = (nota) => {
+    setEstaSonando(nota.nombre);
+    sonidoNota(nota)
+    setTimeout(() => {
+      setEstaSonando("");
+    }, 250);
+  };  
+
+
+  const handleKeyPress = (event) =>{
+    const nota =notas.find(nota=>nota.key===event.key)
+    if (nota){
+       sonidoNota(nota)
+    }
+  }
+  useEffect(()=>{
+    document.addEventListener('keypress', handleKeyPress);
+
+  },[])
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="titulo">
+        <h1><span role="img" aria-label="img">🎶 🎹 </span>Piano con React <span role="img" aria-label="img">🎶</span></h1>
+        <h2>
+          Hecho con <span role="img" aria-label="img">❤️</span> por Rocío
+        </h2>
+        <h3>Esta sonando la nota {estaSonando}</h3>
+        <div 
+          className="esta-sonando">{estaSonando && `suena ${estaSonando}`}</div>
+      </div>
+      <div className="contenedor">
+        {notas.map((nota, i) => (
+          <Key
+            key={i}
+            estaSonando={estaSonando}
+            nota={nota}
+            handleClick={handleClick}
+            handleKeyPress={handleKeyPress}
+
+          />
+        ))}
+      </div>
     </div>
   );
 }
